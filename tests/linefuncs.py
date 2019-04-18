7a1ede3a400d3ad75be6c99b4222af26b2ac36c5
Tobias Sundell <tobsun@student.chalmers.se>
2019-04-18 13:09:10 +0200

    Added a script that demonstrates steering by drawing lines through cones

---
+++ tests/linefuncs.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import cv2
+
+def cross(img, pt, color):
+    cv2.line(img, (pt[0]-5,pt[1]+5), (pt[0]+5,pt[1]-5), color)
+    cv2.line(img, (pt[0]+5,pt[1]+5), (pt[0]-5,pt[1]-5), color)
+
+def findCones(img, hsvLow, hsvHigh):
+    cones = cv2.inRange(img, hsvLow, hsvHigh)
+
+    kernel = np.ones((3, 3), np.uint8)
+    erode = cv2.erode(cones, kernel, iterations=2)
+    dilate = cv2.dilate(erode, kernel, iterations=2)
+
+    contours, _ = cv2.findContours(dilate, cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
+    conePos = []
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        conePos.append((x + int(w/2), y + h))
+    conePos.sort(key=lambda pt: pt[1])
+    return conePos
+
+def addPoints(cones, w, h):
+    pts = cones.copy()
+
+    # add a vertical line at the bottom
+    pts.append((pts[-1][0], h))
+
+    # extend first line to the top edge (y=0)
+    if pts[1][1] - pts[0][1] == 0:
+        pts[0] = (pts[1][0], 0) # line has dy/dx=0; just draw a line to y=0
+    else:
+        slope = (pts[1][0] - pts[0][0]) / (pts[1][1] - pts[0][1])
+        dx = slope * (-pts[0][1])
+        pts[0] = (round(pts[0][0] + dx), 0)
+
+    return pts
+
+def calcMiddleLine(bluPts, ylwPts):
+    if len(bluPts) == 0 or len(ylwPts) == 0: return []
+
+    bluPts = np.array(bluPts)
+    ylwPts = np.array(ylwPts)
+
+    # calculate middle line points from blue points and yellow lines
+    bluCorrPts = np.interp(bluPts[:,1], ylwPts[:,1], ylwPts[:,0])
+    x1 = np.round((bluCorrPts + bluPts[:,0]) / 2).astype(int)
+    pts1 = np.vstack((x1, bluPts[:,1])).T.tolist()
+
+    # calculate middle line points from yellow points and blue lines
+    ylwCorrPts = np.interp(ylwPts[:,1], bluPts[:,1], bluPts[:,0])
+    x2 = np.round((ylwCorrPts + ylwPts[:,0]) / 2).astype(int)
+    pts2 = np.vstack((x2, ylwPts[:,1])).T.tolist()
+
+    pts = [tuple(pt) for pt in (pts1 + pts2)]
+    pts = list(set(pts)) # remove duplicates
+    pts.sort(key=lambda pt: pt[1])
+
+    return pts
+
+def distanceFromMiddle(pts, xMid, y):
+    pts = np.array(pts)
+    x = np.interp(y, pts[:,1], pts[:,0]).item()
+    return round(x - xMid)
---
+++ tests/lines.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import sysv_ipc
+import numpy as np
+import cv2
+import linefuncs as f
+
+# range for identifying blue cones in HSV
+bluLow = (95, 90, 20)
+bluHigh = (130, 255, 255)
+
+# range for identifying yellow cones in HSV
+ylwLow = (23, 60, 100)
+ylwHigh = (28, 255, 255)
+
+# y-coordinate of line to check for steering direction
+ySteering = 100
+
+
+imgPath = "/tmp/img.argb"
+keySharedMemory = sysv_ipc.ftok(imgPath, 1, True)
+keySemMutex = sysv_ipc.ftok(imgPath, 2, True)
+keySemCondition = sysv_ipc.ftok(imgPath, 3, True)
+shm = sysv_ipc.SharedMemory(keySharedMemory)
+mutex = sysv_ipc.Semaphore(keySemMutex)
+cond = sysv_ipc.Semaphore(keySemCondition)
+
+while True:
+    cond.Z()
+
+    mutex.acquire()
+    shm.attach()
+    buf = shm.read()
+    shm.detach()
+    mutex.release()
+
+    img = np.frombuffer(buf, np.uint8).reshape(480, 640, 4)
+    img = img[200:480, 0:640] # remove the top of the image
+
+    blur = cv2.GaussianBlur(img, (5, 5), 0)
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+    pts = np.array(((0, 280), (0, 170), (200, 130), (420, 135), (640, 190),
+        (640, 280), (0, 280))).astype(np.int32)
+    cv2.fillPoly(hsv, [pts], (0, 0, 0)) # black out the car
+
+    bluCones = f.findCones(hsv, bluLow, bluHigh)
+    ylwCones = f.findCones(hsv, ylwLow, ylwHigh)
+    for cone in bluCones:
+        f.cross(img, cone, (255, 0, 0))
+    for cone in ylwCones:
+        f.cross(img, cone, (0, 255, 255))
+
+    if len(bluCones) == 0: bluCones = [(0, 0)]
+    if len(ylwCones) == 0: ylwCones = [(img.shape[1]-1, 0)]
+
+    bluPts = f.addPoints(bluCones, img.shape[1], img.shape[0])
+    ylwPts = f.addPoints(ylwCones, img.shape[1], img.shape[0])
+    midPts = f.calcMiddleLine(bluPts, ylwPts)
+    for i in range(1, len(bluPts)):
+        cv2.line(img, bluPts[i-1], bluPts[i], (255, 0, 0))
+    for i in range(1, len(ylwPts)):
+        cv2.line(img, ylwPts[i-1], ylwPts[i], (0, 255, 255))
+    for i in range(1, len(midPts)):
+        cv2.line(img, midPts[i-1], midPts[i], (0, 255, 0))
+
+    xMid = round(img.shape[1]/2)
+    dx = f.distanceFromMiddle(midPts, round(img.shape[1])/2, ySteering)
+    cv2.arrowedLine(img, (xMid, ySteering), (xMid + dx, ySteering), (0, 0, 255))
+
+    cv2.imshow("img", img)
+    cv2.waitKey(1)
