a9ef0441ac416f8f18b1f373611c0393b68c7ab9
Tobias Sundell <tobsun@student.chalmers.se>
2019-05-07 11:47:03 +0200

    Optimized color ranges and switched blue cones from left to right in lines.py

--- tests/linefuncs.py
+++ tests/linefuncs.py
@@ -7,8 +7,12 @@ def cross(img, pt, color):
     cv2.line(img, (pt[0]-5,pt[1]+5), (pt[0]+5,pt[1]-5), color)
     cv2.line(img, (pt[0]+5,pt[1]+5), (pt[0]-5,pt[1]-5), color)
 
-def findCones(img, hsvLow, hsvHigh):
-    cones = cv2.inRange(img, hsvLow, hsvHigh)
+def findCones(img, hsvRanges):
+    cones = None
+    for i in range(len(hsvRanges)):
+        inRange = cv2.inRange(img, hsvRanges[i][0], hsvRanges[i][1])
+        if i == 0: cones = inRange
+        else:      cones = cv2.bitwise_or(cones, inRange)
 
     kernel = np.ones((3, 3), np.uint8)
     erode = cv2.erode(cones, kernel, iterations=2)
