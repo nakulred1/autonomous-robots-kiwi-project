d8ff2d83507c5eabbd29cce3bb55172aef7a3db6
Nakul R B <nakulred1@gmail.com>
2019-05-14 23:28:22 +0200

    added haar cascading code

--- tests/lines.py
+++ tests/lines.py
@@ -16,7 +16,6 @@ ylwHigh = (28, 255, 255)
 # y-coordinate of line to check for steering direction
 ySteering = 100
 
-
 imgPath = "/tmp/img.argb"
 keySharedMemory = sysv_ipc.ftok(imgPath, 1, True)
 keySemMutex = sysv_ipc.ftok(imgPath, 2, True)
@@ -35,13 +34,13 @@ while True:
     mutex.release()
 
     img = np.frombuffer(buf, np.uint8).reshape(480, 640, 4)
-    img = img[200:480, 0:640] # remove the top of the image
+    img = img[200:480, 0:640]  # remove the top of the image
 
     blur = cv2.GaussianBlur(img, (5, 5), 0)
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
     pts = np.array(((0, 280), (0, 170), (200, 130), (420, 135), (640, 190),
-        (640, 280), (0, 280))).astype(np.int32)
-    cv2.fillPoly(hsv, [pts], (0, 0, 0)) # black out the car
+                    (640, 280), (0, 280))).astype(np.int32)
+    cv2.fillPoly(hsv, [pts], (0, 0, 0))  # black out the car
 
     bluCones = f.findCones(hsv, bluLow, bluHigh)
     ylwCones = f.findCones(hsv, ylwLow, ylwHigh)
@@ -51,23 +50,42 @@ while True:
         f.cross(img, cone, (0, 255, 255))
 
     if len(bluCones) == 0: bluCones = [(0, 0)]
-    if len(ylwCones) == 0: ylwCones = [(img.shape[1]-1, 0)]
+    if len(ylwCones) == 0: ylwCones = [(img.shape[1] - 1, 0)]
 
     bluPts = f.addPoints(bluCones, img.shape[1], img.shape[0])
     ylwPts = f.addPoints(ylwCones, img.shape[1], img.shape[0])
     midPts = f.calcMiddleLine(bluPts, ylwPts)
     for i in range(1, len(bluPts)):
-        cv2.line(img, bluPts[i-1], bluPts[i], (255, 0, 0))
+        cv2.line(img, bluPts[i - 1], bluPts[i], (255, 0, 0))
     for i in range(1, len(ylwPts)):
-        cv2.line(img, ylwPts[i-1], ylwPts[i], (0, 255, 255))
+        cv2.line(img, ylwPts[i - 1], ylwPts[i], (0, 255, 255))
     for i in range(1, len(midPts)):
-        cv2.line(img, midPts[i-1], midPts[i], (0, 255, 0))
+        cv2.line(img, midPts[i - 1], midPts[i], (0, 255, 0))
 
-    xMid = round(img.shape[1]/2)
-    dx = f.distanceFromMiddle(midPts, round(img.shape[1])/2, ySteering)
-    cv2.arrowedLine(img, (xMid, ySteering), (xMid + dx, ySteering), (0, 0, 255))
+    xMid = round(img.shape[1] / 2)
+    dx = f.distanceFromMiddle(midPts, round(img.shape[1]) / 2, ySteering)
+    cv2.arrowedLine(img, (xMid, ySteering), (xMid + dx, ySteering),
+                    (0, 0, 255))
+
+    #Object detection using haar cascading
+    face_cascade = cv2.CascadeClassifier('cascade.xml')
+
+    def detect_face(img):
+        face_img = img.copy()
+
+        face_rects = face_cascade.detectMultiScale(face_img)
+
+        for (x, y, w, h) in face_rects:
+            cv2.rectangle(face_img, (x, y), (x + w, y + h), (255, 255, 255),
+                          10)
+        return face_img
+
+    img = detect_face(img)
 
     cv2.imshow("img", img)
 
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
 
-    cv2.waitKey(1)
+cv2.destroyAllWindows()
