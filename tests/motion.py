aa458d1c1bc26501f9b0edf8de3d7312e9f1c838
Tobias Sundell <tobsun@student.chalmers.se>
2019-05-15 17:31:04 +0200

    Added simple motion detection test

---
+++ tests/motion.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import sysv_ipc
+import numpy as np
+import cv2
+import linefuncs as f
+
+imgPath = "/tmp/img.argb"
+keySharedMemory = sysv_ipc.ftok(imgPath, 1, True)
+keySemMutex = sysv_ipc.ftok(imgPath, 2, True)
+keySemCondition = sysv_ipc.ftok(imgPath, 3, True)
+shm = sysv_ipc.SharedMemory(keySharedMemory)
+mutex = sysv_ipc.Semaphore(keySemMutex)
+cond = sysv_ipc.Semaphore(keySemCondition)
+
+fgbg = cv2.createBackgroundSubtractorMOG2(varThreshold=60, detectShadows=False)
+
+while True:
+    cond.Z()
+
+    mutex.acquire()
+    shm.attach()
+    buf = shm.read()
+    shm.detach()
+    mutex.release()
+
+    img = np.frombuffer(buf, np.uint8).reshape(480, 640, 4)
+    img = img[200:380, 0:640] # remove the top and bottom of the image
+
+    blur = cv2.GaussianBlur(img, (5, 5), 0)
+    motion = fgbg.apply(blur)
+
+    cv2.imshow("motion", motion)
+    cv2.waitKey(1)
